a5a8fd5e6233a8c3954925be48139a874ad4dbde
Berkan <Berkan@DESKTOP-1A3R6E7>
2024-10-07 21:46:01 +0200

    preparing for some upcoming additions.

--- r5sw/r5sw.cpp
+++ r5sw/r5sw.cpp
@@ -2,24 +2,31 @@
 
 SPOOF_CALL_METHOD(protected_func, do_spoof_call_test, std::uint64_t, std::uint64_t, std::uint64_t, std::uint64_t, std::uint64_t, std::uint64_t, std::uint64_t)
 
+namespace tests
+{
+    void basic()
+    {
+        printf("------( trying to call from process itself... )------\n");
+        protected_func();
+        printf("-----------------------------------------------------\n");
+
+        printf("------( trying to call from RWX page... )------\n");
+        tools::make_call(protected_func);
+        printf("-----------------------------------------------------\n");
+
+        printf("------( trying to call with spoof (namazso): )------\n");
+        spoof_call(&namazso_gadget, protected_func);
+        printf("-----------------------------------------------------\n");
+
+        printf("------( trying to call with spoof (beakers): )------\n");
+        prepare_proxy_for_module((std::uint8_t*)LoadLibraryA("kernel32.dll"));
+        do_spoof_call_test(0, 0, 0, 0, 0, 0);
+        printf("-----------------------------------------------------\n");
+    }
+}
+
 int main()
 {
-    printf("------( trying to call from process itself... )------\n");
-    protected_func();
-    printf("-----------------------------------------------------\n");
-
-    printf("------( trying to call from RWX page... )------\n");
-    tools::make_call(protected_func);
-    printf("-----------------------------------------------------\n");
-
-    printf("------( trying to call with spoof (namazso): )------\n");
-    spoof_call(&namazso_gadget, protected_func);
-    printf("-----------------------------------------------------\n");
-
-    printf("------( trying to call with spoof (beakers): )------\n");
-    prepare_proxy_for_module((std::uint8_t*)LoadLibraryA("kernel32.dll"));
-    do_spoof_call_test(0, 0, 0, 0, 0, 0);
-    printf("-----------------------------------------------------\n");
-    
+    tests::basic( );
     return getchar();
 }
